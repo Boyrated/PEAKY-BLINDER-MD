bfa5152e97b0780ff7bef4b004e1ad2b4b762f34
Boyrated <nevillekimutai51@gmail.com>
2026-07-06 14:16:19 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -4,9 +4,9 @@
 
 module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  OWNER_NAME: process.env.OWNER_NAME || 'Neville', 
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '254718477763', 
+  SESSION_ID: process.env.SESSION_ID || 'blinder~SHQ1RQxT#lI-HqU5mPIiLrC_AoJtP-JZnkF1XFh--IATawtwYhh4', // paste here your blinders session ID
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
